d839ceb12d22ad0d54c01f727c0cc3c097c577d9
cbphoto720 <cbphoto720@gmail.com>
2024-08-06 17:02:49 -0700

    Clean up base code from example

--- Code/BASE/BASE.ino
+++ Code/BASE/BASE.ino
@@ -21,64 +21,52 @@
 #include <RFM69.h>
 #include <RFM69_ATC.h>
 #include <SPI.h>
+#include <mavlink.h>
 
+/* RFM */
 #define NETWORKID     10   // Must be the same for all nodes (0 to 255)
 #define MYNODEID      0   // My node ID (0 to 255)
 #define TONODEID      1   // Destination node ID (0 to 254, 255 = broadcast)
 #define MY_RF69_IRQ_PIN 8
 #define MY_RF69_SPI_CS 21
 
-// RFM69 frequency, uncomment the frequency of your module:
-
-//#define FREQUENCY   RF69_433MHZ
 #define FREQUENCY     RF69_915MHZ
-
-// AES encryption (or not):
-
+// AES encryption
 #define ENCRYPT       true // Set to "true" to use encryption
 #define ENCRYPTKEY    "FILEDCREW1234567" // Use the same 16-byte key on all nodes
-
-// Use ACKnowledge when sending messages (or not):
-
 #define USEACK        true // Request ACKs or not
-
-// Use Automatic Transmisionn Control (ATC) to transmit power at close range
-
 #define ENABLE_ATC  //comment out this line to disable AUTO TRANSMISSION CONTROL
 #define ATC_RSSI -80
 
-// Packet sent/received indicator LED (optional):
-
-// #define LED           9 // LED positive pin
-// #define GND           8 // LED ground pin
-
 // Create a library object for our RFM69HCW module:
-
 #ifdef ENABLE_ATC
 RFM69_ATC radio(MY_RF69_SPI_CS,MY_RF69_IRQ_PIN);
 #else
 RFM69 radio(MY_RF69_SPI_CS,MY_RF69_IRQ_PIN);
 #endif
 
+/* MAVlink */
+#define SYS_ID 1
+#define COMP_ID 1
+
+#define mavlinkSerial Serial
+
+bool parseGPSData(const char* gpsData, int &drifterID, char* utcTime, float &lat, float &lon, float &voltage) {
+  // Assuming GPS data is received as a comma-separated string "DrifterID#,UTCtime,Lat,Lon,Voltage"
+  int parsed = sscanf(gpsData, "%d,%19[^,],%f,%f,%f", &drifterID, utcTime, &lat, &lon, &voltage);
+  return (parsed == 5);
+}
+
 void setup()
 {
-  // Open a serial port so we can send keystrokes to the module:
-  
+  mavlinkSerial.begin(57600); // MAVLink baud rate
   Serial.begin(9600);
   Serial.print("Node ");
   Serial.print(MYNODEID,DEC);
   Serial.println(" ready");  
-
-  // Set up the indicator LED (optional):
-  
-  // pinMode(LED,OUTPUT);
-  // digitalWrite(LED,LOW);
-  // pinMode(GND,OUTPUT);
-  // digitalWrite(GND,LOW);
-    
+   
   // Initialize the RFM69HCW:
-  //  radio.setCS(10);  //uncomment if using Pro Micro
-    int c1 = 0;
+  int c1 = 0;
   Serial.print("Initializing");
   while (!radio.initialize(FREQUENCY, MYNODEID, NETWORKID)) {
     Serial.print(".");
@@ -94,13 +82,13 @@ void setup()
   radio.setHighPower(); // Always use this for RFM69HCW
 
   // Turn on encryption if desired:
-  
   if (ENCRYPT)
     radio.encrypt(ENCRYPTKEY);
+  #ifdef ENABLE_ATC
+    radio.enableAutoPower(ATC_RSSI);
+  #endif
 
-#ifdef ENABLE_ATC
-  radio.enableAutoPower(ATC_RSSI);
-#endif
+  Serial.println("Starting GPS to MAVLink bridge...");
 }
 
 void loop()
@@ -126,15 +114,10 @@ void loop()
     {
       sendbuffer[sendlength] = input;
       sendlength++;
-    }
-
-    // If the input is a carriage return, or the buffer is full:
-    
+    }    
     if ((input == '\r') || (sendlength == 61)) // CR or buffer full
     {
       // Send the packet!
-
-
       Serial.print("sending to node ");
       Serial.print(TONODEID, DEC);
       Serial.print(": [");
@@ -142,70 +125,45 @@ void loop()
         Serial.print(sendbuffer[i]);
       Serial.println("]");
       
-      // There are two ways to send packets. If you want
-      // acknowledgements, use sendWithRetry():
-      
       if (USEACK)
       {
         if (radio.sendWithRetry(TONODEID, sendbuffer, sendlength, 10))
           Serial.println("ACK received!");
         else
           Serial.println("no ACK received :(");
-      }
-
-      // If you don't need acknowledgements, just use send():
-      
+      }      
       else // don't use ACK
       {
         radio.send(TONODEID, sendbuffer, sendlength);
       }
-      
       sendlength = 0; // reset the packet
-      // Blink(LED,10);
     }
   }
 
   // RECEIVING
-
-  // In this section, we'll check with the RFM69HCW to see
-  // if it has received any packets:
-
   if (radio.receiveDone()) // Got one!
-  {
-    // Print out the information:
-    
+  {    
     Serial.print("(Drifter ");
     Serial.print(radio.SENDERID, DEC);
     Serial.print("): [");
-
-    // The actual message is contained in the DATA array,
-    // and is DATALEN bytes in size:
     
     for (byte i = 0; i < radio.DATALEN; i++)
       Serial.print((char)radio.DATA[i]);
-
-    // RSSI is the "Receive Signal Strength Indicator",
-    // smaller numbers mean higher power.
-    
+          
     Serial.print("], RSSI ");
     Serial.println(radio.RSSI);
-
-    // Send an ACK if requested.
-    // (You don't need this code if you're not using ACKs.)
     
     if (radio.ACKRequested())
     {
       radio.sendACK();
       Serial.println("ACK sent");
     }
-    // Blink(LED,10);
   }
+  
+  int drifterID;
+    char utcTime[20];
+    float lat, lon, voltage;
+    if (parseGPSData(gpsData, drifterID, utcTime, lat, lon, voltage)) {
+      sendGPSData(lat, lon);
+    }
 }
-
-// void Blink(byte PIN, int DELAY_MS)
-// // Blink an LED for a given number of ms
-// {
-//   digitalWrite(PIN,HIGH);
-//   delay(DELAY_MS);
-//   digitalWrite(PIN,LOW);
-// }
